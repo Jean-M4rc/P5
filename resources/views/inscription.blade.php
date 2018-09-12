f35bcc80cfef1ac6bc5dfa70e2ebf2acd520b002
Jean-Marc <jeanmarc.voisin.tai@gmail.com>
2018-09-12 12:13:19 +0200

    Perfectionnement du formulaire d'inscription. Ajout des astérisques rappel de requierement. Message d'informations.

--- resources/views/inscription.blade.php
+++ resources/views/inscription.blade.php
@@ -4,33 +4,35 @@
 
     <form action="nouvelleInscription.php" method="post" class="container mb-5">
         <fieldset><!-- Formulaire utilisateur commun -->
-            <legend class="mt-2">Inscription à Iticourt</legend>            
+            <legend class="mt-2 text-center">Inscription à Iticourt</legend>            
             <div class="form-group"><!-- mail -->
-                <label>Adresse mail</label>
-                <input class="form-control" name="email" aria-describedby="emailHelp" placeholder="Email" type="email">
+                <label>Adresse mail <span style="color:red">*</span></label>
+                <input class="form-control" name="email" aria-describedby="emailHelp" placeholder="Email" type="email" required>
                 <small id="emailHelp" class="form-text text-muted">Nous ne transmettrons jamais votre email à un tiers.</small>
             </div>
             <div class="form-group"><!-- prénom -->
-                <label>Prénom</label>
-                <input class="form-control" name="nickname" placeholder="Prénom" type="text">
+                <label>Prénom / Pseudo <span style="color:red">*</span></label>
+                <input class="form-control" name="nickname" placeholder="Prénom" type="text" required>
+                <small id="nicknameHelp" class="form-text text-muted">Minimum 4 caractères.</small>
             </div>
             <div class="form-group"><!-- nom -->
                 <label>Nom</label>
                 <input class="form-control" name="firstname" placeholder="Nom" type="text">
             </div>
             <div class="form-group"><!-- passwd -->
-                <label for="InputPassword1">Mot de passe</label>
-                <input class="form-control" id="InputPassword1" name="password" placeholder="Mot de passe" type="password">
+                <label for="InputPassword1">Mot de passe <span style="color:red">*</span></label>
+                <input class="form-control" id="InputPassword1" name="password" placeholder="Mot de passe" type="password" required>
             </div>
             <div class="form-group"><!-- passwd_confirmation -->
-                <label for="InputPassword2">Confirmer votre mot de passe</label>
-                <input class="form-control" id="InputPassword2" name="password_confirmation" placeholder="Mot de passe" type="password">
+                <label for="InputPassword2">Confirmer votre mot de passe <span style="color:red">*</span></label>
+                <input class="form-control" id="InputPassword2" name="password_confirmation" placeholder="Mot de passe" type="password" required>
             </div>
-            <button type="submit" class="btn btn-primary">M'inscrire</button>            
+            <small class="form-text text-muted"><span style="color:red">*</span> : Champs requis.</small><br/>
+            <button type="submit" class="btn btn-primary btn-lg btn-block">M'inscrire</button>
         </fieldset>
         <hr>
-        <fieldset><!-- Complément formulaire pour les vendeurs -->
-            <p><button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#sellercollapse" aria-expanded="false" aria-controls="collapseExample">Vendeur ?</button></p>
+        <fieldset id="seller_confirmation_check"><!-- Complément formulaire pour les vendeurs -->
+            <p><button class="btn btn-dark" type="button" data-toggle="collapse" data-target="#sellercollapse" aria-expanded="false" aria-controls="collapseExample">Vendeur ?</button></p>
             
             <div class="collapse container" id="sellercollapse">
                 <div class="alert alert-dismissible alert-primary">
@@ -38,30 +40,30 @@
                     <strong>N'oubliez pas de remplir les informations précédentes.</strong>
                 </div>
                 <legend>Vendeur</legend>
-                <div class="form-group"><!-- checkbox de confirmation de vente -->
+                <div class="form-group alert alert-info"><!-- checkbox de confirmation de vente -->
                     <div class="custom-control custom-checkbox">
                         <input class="custom-control-input" id="seller_confirmation" type="checkbox" name="seller_confirmation">
                         <label class="custom-control-label" for="seller_confirmation">Je déclare être un vendeur</label>
                     </div>
                 </div>
                 <div class="form-group"><!-- Nom de la structure -->
-                    <label class="col-form-label" for="inputBusinessName">Le nom de votre structure</label>
+                    <label class="col-form-label" for="inputBusinessName">Le nom de votre structure <span style="color:#772953">*</span></label>
                     <input class="form-control" placeholder="Les volailles de Roseline..." id="inputBusinessName" type="text" name="businessName">
                 </div>
                 <div class="form-group"><!-- Téléphone -->
                     <div class="input-group mb-1">
                         <div class="input-group-prepend">
-                            <span class="input-group-text">Téléphone :</span>
+                            <span class="input-group-text">Téléphone <sup style="color:#772953">*</sup>:</span>
                         </div>
                         <input class="form-control" type="text">
                     </div>
                 </div>
                 <div class="form-group"><!-- Adresse -->
-                    <label>Adresse :</label>
+                    <label>Adresse <span style="color:#772953">*</span></label>
                     <textarea class="form-control" id="address" rows="3"></textarea>
                 </div>
                 <div class="form-group row"><!-- Sélection des catégories -->
-                    <label aria-describedby="categoriesHelp" class="col-12">Vos produits<small id="categoriesHelp" class="form-text text-muted">Vous pouvez sélectionner plusieurs catégories.</small></label>
+                    <label aria-describedby="categoriesHelp" class="col-12">Vos produits <span style="color:#772953">*</span><small id="categoriesHelp" class="form-text text-muted">Vous pouvez sélectionner plusieurs catégories.</small></label>
                     <div class="custom-control custom-checkbox col-12 col-sm-6 col-md-3">
                         <input class="custom-control-input" id="product_category1" type="checkbox" name="product_category1">
                         <label class="custom-control-label" for="product_category1">Fruits & Légumes</label>
@@ -96,7 +98,7 @@
                     </div>
                 </div>
                 <div class="form-group row"><!-- Récupération de la position GPS du point de vente -->
-                    <label aria-describedby="gpsHelp" class="col-12">Vos coordonnées GPS<small id="gpsHelp" class="form-text text-muted">Indiquez les coordonnées GPS de votre point de vente, ou de l'entrée de votre propriété sur la voie publique</small></label>
+                    <label aria-describedby="gpsHelp" class="col-12">Vos coordonnées GPS <span style="color:#772953">*</span><small id="gpsHelp" class="form-text text-muted">Indiquez les coordonnées GPS de votre point de vente, ou de l'entrée de votre propriété sur la voie publique</small></label>
                     <div class="form-group container">
                         <input class="form-control mb-1" id="longInput" name="long" placeholder="Longitude ex : 49.3154287" type="text">
                         <input class="form-control mb-1" id="latInput" name="lat" placeholder="Latitude ex : -0.875458754" type="text">
@@ -104,14 +106,14 @@
                     </div>
                 </div>
                 <div class="form-group"><!-- Présentation de la structure -->
-                    <label>Présentation de votre production<small class="form-text text-muted">Décrivez briévement les produits que vous souhaitez vendre.</small></label>
+                    <label>Présentation de votre production <span style="color:#772953">*</span><small class="form-text text-muted">Décrivez briévement les produits que vous souhaitez vendre.</small></label>
                     <textarea class="form-control" id="teaserSeeler" rows="3"></textarea>
                 </div>
                 <div class="form-group"><!-- Photos -->
                     <label>Ajoutez quelques photos pour présenter votre structure et vos produits.</label>
                     <div  class="custom-file">
                         <input class="custom-file-input" id="InputFile1" type="file" aria-describedby="fileHelp" name="avatar">
-                        <label class="custom-file-label" for="InputFile1">Photo principale :</label>
+                        <label class="custom-file-label" for="InputFile1">Photo principale : <span style="color:#772953">*</span></label>
                         <small id="fileHelp" class="form-text text-muted">Ajouter une photo qui réprésente votre structure ou l'entrée de votre point de vente pour aider vos visiteurs.</small>
                     </div>
                     <div  class="custom-file mt-2 mb-1">
@@ -127,8 +129,11 @@
                         <label class="custom-file-label" for="InputFile4">Photo alternative 3 :</label>
                     </div>
                     <small id="fileHelp" class="form-text text-muted">Ajouter des photos de vos produits.</small>
+                    <small id="requirementHelp" class="form-text text-muted"><span style="color:#772953">*</span> : Champs requis pour s'inscrire en tant que vendeur.</small>
                 </div>
-                <button type="submit" class="btn btn-primary">M'inscrire</button>
+                <div class="alert alert-warning">Attention si <a href="#seller_confirmation_check">"Je déclare être un vendeur"</a> n'est pas cochée vos informations de vendeur ne seront pas enregistrées et vous serez inscrit en tant que simple acheteur.</div>
+                
+                <button type="submit" class="btn btn-primary btn-lg btn-block">M'inscrire</button>
             </div>
         </fieldset>
     </form>
